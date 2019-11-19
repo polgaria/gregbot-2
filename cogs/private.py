82730c40011c2b85c5b10c969ed8afb109ddbfb2
greg <grxxgbonesesh@gmail.com>
2019-11-19 19:15:33 -0300

    added logout command

---
+++ cogs/private.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+cog_name = 'Private'
+class Private(commands.Cog):
+
+    def __init__(self, greg):
+        self.greg = greg
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{cog_name} was loaded.')
+
+    @commands.command(aliases=['kill'])
+    @commands.is_owner()
+    async def logout(self, ctx):
+        await ctx.send('Bye Mom !!!')
+        await self.greg.logout()
+
+def setup(greg):
+    greg.add_cog(Private(greg))
